eaa01447229f7207e6357d17c872d5e6db24029e
mcastillo23 <mac525@georgetown.edu>
2021-03-04 13:35:38 -0500

    Fetching trading prices for output

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -17,25 +17,39 @@ if symbol.isalpha() == False or len(symbol) > 5:
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
 
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
+
+if "Error Message" in response.text:
+    print("Sorry, couldn't find any trading data for that stock symbol.")
+    exit()
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
 
-#print("-------------------------")
-#print("SELECTED SYMBOL: XYZ")
-#print("-------------------------")
-#print("REQUESTING STOCK MARKET DATA...")
-#print("REQUEST AT: 2018-02-20 02:00pm")
-#print("-------------------------")
-#print("LATEST DAY: 2018-02-20")
-#print("LATEST CLOSE: $100,000.00")
-#print("RECENT HIGH: $101,000.00")
-#print("RECENT LOW: $99,000.00")
-#print("-------------------------")
-#print("RECOMMENDATION: BUY!")
-#print("RECOMMENDATION REASON: TODO")
-#print("-------------------------")
-#print("HAPPY INVESTING!")
-#print("-------------------------")
+time_series = parsed_response["Time Series (Daily)"]
+dates = list(time_series.keys())
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = time_series[dates[0]]["4. close"]
+latest_close = time_series[dates[0]]["4. close"]
+
+high_prices = [float(time_series[date]["2. high"]) for date in dates]
+low_prices = [float(time_series[date]["3. low"]) for date in dates]
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+print("-------------------------")
+print(f"SELECTED SYMBOL: {symbol}")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {latest_close}")
+print(f"RECENT HIGH: {recent_high}")
+print(f"RECENT LOW: {recent_low}")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
