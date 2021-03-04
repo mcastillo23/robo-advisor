feed78bc8dea4037a933baa8e4d6011c57cc4809
mcastillo23 <mac525@georgetown.edu>
2021-03-04 02:27:50 -0500

    Fetching Data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,12 +1,24 @@
 # this is the "app/robo_advisor.py" file
 
+import requests
+import json
+
 symbol = input("Please input a sotck or cryptocurrency symbol.")
 
 if symbol.isalpha() == False or len(symbol) > 5:
     print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
     exit()
 
-print("-------------------------")
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+
+response = requests.get(request_url)
+print(type(response))
+print(response.status_code)
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response))
+
+#print("-------------------------")
 #print("SELECTED SYMBOL: XYZ")
 #print("-------------------------")
 #print("REQUESTING STOCK MARKET DATA...")
